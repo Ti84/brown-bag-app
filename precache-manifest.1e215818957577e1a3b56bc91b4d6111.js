e589fb0c86c77c24cec490ac42e23d958bb43239
Shane4666 <sc897@nau.edu>
2020-03-20 15:13:18 -0700

    Updates

---
+++ precache-manifest.1e215818957577e1a3b56bc91b4d6111.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "05a4b07930a9b56db365da7fd90165be",
+    "url": "/brown-bag-app/index.html"
+  },
+  {
+    "revision": "7861d88e333e5fd65642",
+    "url": "/brown-bag-app/static/css/main.2d258b76.chunk.css"
+  },
+  {
+    "revision": "b36475480f288e7eef0f",
+    "url": "/brown-bag-app/static/js/2.fcde08c6.chunk.js"
+  },
+  {
+    "revision": "45c3b0e6a77e536ad25f6ffa575bb86a",
+    "url": "/brown-bag-app/static/js/2.fcde08c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7861d88e333e5fd65642",
+    "url": "/brown-bag-app/static/js/main.6e5ee92e.chunk.js"
+  },
+  {
+    "revision": "43847d3472de17568fa9",
+    "url": "/brown-bag-app/static/js/runtime-main.01e618cc.js"
+  },
+  {
+    "revision": "14be1369eed87ccff283f220f0e3e16a",
+    "url": "/brown-bag-app/static/media/codepen.14be1369.svg"
+  },
+  {
+    "revision": "617870e422d4b75087cdcf9ff298032e",
+    "url": "/brown-bag-app/static/media/github.617870e4.svg"
+  },
+  {
+    "revision": "be06689853aec102607f5e54e6fd2dfa",
+    "url": "/brown-bag-app/static/media/twitter.be066898.svg"
+  }
+]);
