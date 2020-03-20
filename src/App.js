7cdf095651c81b7aaa23ac2b3607b6af41686ab4
Shane4666 <sc897@nau.edu>
2020-03-20 15:26:59 -0700

    sup

--- src/App.js
+++ src/App.js
@@ -5,11 +5,19 @@ import './App.css';
 
 import PEOPLE from './data/people';
 
-// To Add:
-// State example to main app.
-// Sorting / Filters?
-// Following option for each profile card
-// Make flow chart to show data / rerender flow
+
+// To Do:
+// 1. Create script for live coding demo
+// 2. Create startup app with css / etc so you only have to live code the react parts
+// 3. Code profile card first in app component
+// 4. Move profile card to its own component and show how it could become reusable
+// 5. Create a dark / light mode toggle component to display how you manage state / pass functions to child
+      // Demonstrate useState, etc.
+
+// If you don't do it in the app, make sure to explain useEffect and explain how you can create your own custom hooks.
+
+// REMEMBER YOU ONLY HAVE AN HOUR BRO
+
 const App = () => {
   return (
     <div className="app">
